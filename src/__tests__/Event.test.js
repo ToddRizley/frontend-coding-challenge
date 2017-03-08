4363ccbcc5784d369f2642f8501f1d4cd9035344
ToddRizley <todd.rizley@gmail.com>
2017-03-08 16:39:07 -0500

    update file structure

--- src/__tests__/Event.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme'
-import ReactDOM from 'react-dom';
-import Event from '../Event';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Event />, div);
-});
